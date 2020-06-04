01a4ffdf7821dc91967db0a07b36a4a25e3c431f
nikabutikashvili <65020676+nikabutikashvili@users.noreply.github.com>
2020-06-04 06:56:09 +0400

    email and password validation

--- login.js
+++ login.js
@@ -1,48 +1,42 @@
 user1 = {
-    username: "John2233",
+    email: "John2233@mail.com",
     password: "1234"
 };
 user2 = {
-    username: "Bob443",
+    email: "Bob443@yahoo.com",
     password: "2356"
 };
 user3 = {
-    username: "Jack665",
+    email: "Jack665@mail.com",
     password: "7788"
 };
 user4 = {
-    username: "Kate776",
-    password: "998800022"
+    email: "Kate776@email.com",
+    password: "Geolab12#$"
 };
 user5 = {
-    username: "Nick887",
+    email: "Nick887@mail.com",
     password: "6644"
 };
 
 users = [user1, user2, user3, user4, user5];
 
 
-function checkUsernameAndPassword(possibleUsername, possiblePassword) {
+function checkEmailAndPassword(possibleEmail, possiblePassword) {
     let user = {
-        username: '',
+        email: '',
         password: ''
     }
     for (let i = 0; i < users.length; i++) {
-        if(possibleUsername === users[i].username){
+        if(possibleEmail === users[i].email){
             user = users[i];
         }
     }
-    if(!user.username) {
+    if(!user.email) {
         alert("მომხმარებელი არ მოიძებნა");
     } else {
         if(possiblePassword === user.password) {
-            if(user.password.length < 8) {
-                alert("შეხვედით სისტემაში წარმატებით, თუმცა გთხოვთ შეცვალოთ პაროლი");
-                window.location = "index.html";
-            } else {
-                alert("შეხვედით სისტემაში წარმატებით");
-                window.location = "index.html";
-            }
+            CheckPassword(possiblePassword);    
         } else {
             alert("პაროლი არასწორია");
         }
@@ -50,9 +44,11 @@ function checkUsernameAndPassword(possibleUsername, possiblePassword) {
 }
 
 function logOn() {
-    let possibleUsername = document.getElementById("username").value;
+    let possibleUsername = document.getElementById("email").value;
     let possiblePassword = document.getElementById("password").value;
-    checkUsernameAndPassword(possibleUsername, possiblePassword);
+    if(validateEmail()) {
+        checkEmailAndPassword(possibleUsername, possiblePassword);
+    }
 }
 
 let showPasswordButton = document.querySelector('#showPasswordButton');
@@ -61,3 +57,23 @@ showPasswordButton.addEventListener('click', function (e) {
     let type = password.getAttribute('type') === 'password' ? 'text' : 'password';
     password.setAttribute('type', type);
 });
+
+function validateEmail() 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(document.getElementById("email").value))
+  {
+    return true
+  }
+    alert("იმეილი არის არასწორ ფორმატში, გთხოვთ გაასწოროთ")
+    return false
+}
+
+function CheckPassword(input) { 
+    let password =  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+    if(input.match(password)) { 
+    alert('შეხვედით სისტემაში წარმატებით')
+    }
+    else { 
+    alert('შეხვედით სისტემაში წარმატებით, თუმცა გთხოვთ შეცვალოთ პაროლი')
+    }
+}
