f9921bbf9e491be4274b60cf5726eb3f948f94fe
nikabutikashvili <65020676+nikabutikashvili@users.noreply.github.com>
2020-06-21 16:13:16 +0400

    Binary Search

---
+++ binarysearch/binarySearch.js
@@ -0,0 +1,18 @@
+const array = [2, 3, 6, 8, 12, 17, 25, 31];
+function binarySearch(number, array) {
+    let startIndex = 0;
+    let endIndex = array.length - 1;
+    while(endIndex >= startIndex) {
+        let middleIndex = Math.floor(startIndex + ((endIndex - startIndex)/2));
+        if(array[middleIndex] === number) {
+            return middleIndex;
+        } else if(middleIndex < number) {
+            startIndex = middleIndex + 1;
+        } else {
+            endIndex = middleIndex - 1;
+        }
+    }
+    return -1;
+}
+
+console.log(binarySearch(8, array));
